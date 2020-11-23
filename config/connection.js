1a5abce346ef0fdfbc0ebf36c1a7462cc45a2e5e
Todd Jones <jonesgtodd@gmail.com>
2020-11-22 19:58:50 -0600

    add code in  connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,33 @@
 const mysql = require("mysql");
 const connection;
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB)
-}
+//creating connection
+// var connection = mysql.createConnection({ // Set connection parameters
+//   host: "localhost",
+//   user: "root",
+//   password: "2007M#lrose1029",
+//   database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL) {            
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({ // Set local connection parameters
+    host: "localhost",
+    user: "root",
+    password: "2007M#lrose1029",
+    database: "burgers_db"
+    });
+};
+
+//creating connection
+connection.connect(function(err) {     
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//exporting for ORM
+module.exports = connection;  
