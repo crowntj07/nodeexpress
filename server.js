aef4c3199a3f2bf32bffbb3cff5c8a7db7a2e63f
Todd Jones <jonesgtodd@gmail.com>
2020-11-23 18:57:33 -0600

    updated the server.js file added function code

--- server.js
+++ server.js
@@ -5,3 +5,30 @@ const exphbs = require("express-handlebars");
                               
 const bodyParser = require("body-parser");                            
 
+
+let routes = require("./controllers/burgers_controller.js");      
+
+let app = express();                       
+//local host port 2002                        
+let PORT = process.env.PORT || 2002;                              
+
+
+app.use(express.static("public"));                                
+
+
+app.use(bodyParser.json());                                         
+app.use(bodyParser.urlencoded({ extended: true }));                 
+
+
+app.engine("handlebars", exphbs({                                 
+    defaultLayout: "main"                                        
+}));
+
+app.set("view engine", "handlebars");                               
+
+
+app.use(routes);                                                 
+
+app.listen(PORT, function(){                             
+console.log("Listening on Port: " + PORT);
+});
